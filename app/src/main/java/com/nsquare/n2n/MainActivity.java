99db73a4b530e7ca9c383983492302fb47278150
Nishanthan Ravichandran <nishanthravi226@gmail.com>
2023-05-28 19:51:24 +0530

    Initial project setup

---
+++ app/src/main/java/com/nsquare/n2n/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nsquare.n2n;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/* This activity is going to be the Splash Screen which will last for 3 seconds*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
